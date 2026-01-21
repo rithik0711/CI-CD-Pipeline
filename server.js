36357a28dcfb5c82bb8efa19771295ff5147a5eb
Rithikeswaran M <151950168+rithik0711@users.noreply.github.com>
2026-01-21 14:35:40 +0530

    Modify Server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "CI/CD Pipeline is working!" });
+  res.json({ message: "CI/CD Pipeline is working in port 3000!" });
 });
 
 app.get("/health", (req, res) => {
